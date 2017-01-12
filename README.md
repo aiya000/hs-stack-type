6f539aeecae0a3d1c52f5fa1ae3b71e155a5e069
aiya000 <aiya000.develop@gmail.com>
2017-01-13 02:13:13 +0900

    Create all

---
+++ README.md
@@ -0,0 +1,5 @@
+# hs-stack-type
+The very very simply representation of the stack data type.
+
+## Attention
+This library isn't related to haskell-stack.
---
+++ src/Data/Stack.hs
@@ -0,0 +1,22 @@
+-- | The basic stack type
+module Data.Stack where
+
+import Control.Monad.Trans.State.Lazy (State, get, put, runState)
+
+type Stack s a = State [s] a
+
+
+push :: s -> Stack s ()
+push x = do
+  xs <- get
+  put $ x:xs
+  return ()
+
+pop :: Stack s s
+pop = do
+  (x:xs) <- get
+  put xs
+  return x
+
+runStack :: Stack s a -> [s] -> (a, [s])
+runStack = runState
